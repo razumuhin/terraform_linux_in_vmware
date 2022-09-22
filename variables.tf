7fae27d9579249bc71b0f7abb59c5f936fde996e
ersin kaya <77919896+razumuhin@users.noreply.github.com>
2022-09-22 21:37:01 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,90 @@
+
 variable "admin_user_name"{
-    type = string
+    type = map(string)
 }
 
 variable "admin_password"{
-    type = string
+    type = map(string)
 }
 
-variable "user_name"{
-    type = string
+variable "provider_user_name"{
+    type = map(string)
 }
 
-variable "password"{
-    type = string
+variable "provider_password"{
+    type = map(string)
 }
 
 variable "vsphere_server_name"{
-    type = string
+    type = map(string)
 }
 
 variable "instance_count"{
-    type = number
+    type = map(number)
 }
-variable "new_user_publickey" {
-    type = string
+
+variable "ersinkaya_publickey" {
+    type = map(string)
 }
+
+variable "datacenter" {
+  type = map(string)
+}
+
+variable "datastore_cluster" {
+  type = map(string)
+}
+
+variable "datastore" {
+  type = map(string)
+}
+
+variable "resource_pool" {
+  type = map(string)
+}
+
+variable "network" {
+  type = map(string)
+}
+
+variable "vm_name_prefix" {
+  type = map(string)
+}
+
+variable "vm_folder" {
+  type = map(string)
+}
+
+variable "num_of_cpus" {
+  type = map(number)
+}
+
+variable "memory_size" {
+  type = map(number)
+}
+
+variable "disk_label" {
+  type = map(string)
+}
+
+variable "domain" {
+  type = map(string)
+}
+
+variable "ipv4_address_prefix" {
+  type = map(string)
+}
+
+variable "ipv4_net_mask" {
+  type = map(number)
+}
+
+variable "ipv4_gateway" {
+  type = map(string)
+}
+
+variable "dns_server_list" {
+  type = map(list(string))
+}
+
+
