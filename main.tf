576a1663bd471fccda2a005b4258c86d01058690
ersin kaya <77919896+razumuhin@users.noreply.github.com>
2022-09-29 20:25:10 +0300

    vmmachine name edit, second disk opsian added.

--- main.tf
+++ main.tf
@@ -26,13 +26,13 @@ data "vsphere_network" "network" {
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "TEMPLATE_NAME IN YOUR VMWARE TEMPLATE FOLDER"
+  name          = var.template_name[terraform.workspace]
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
   count = var.instance_count[terraform.workspace]
-  name             = "${var.vm_name_prefix[terraform.workspace]}-${count.index + 1}"
+  name             = var.machine_name_start_index[terraform.workspace] + count.index + 1<100 ? "${var.ipv4_address_prefix[terraform.workspace]}.0${var.machine_name_start_index[terraform.workspace] + count.index + 1}-${var.vm_name_prefix[terraform.workspace]}-${count.index + 1}" : "${var.ipv4_address_prefix[terraform.workspace]}.${var.machine_name_start_index[terraform.workspace] + count.index}-${var.vm_name_prefix[terraform.workspace]}-${count.index + 1}"
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_cluster_id = data.vsphere_datastore_cluster.datastore_cluster.id
   folder           = var.vm_folder[terraform.workspace]
@@ -53,6 +53,12 @@ resource "vsphere_virtual_machine" "vm" {
     eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
     thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
   }
+   # //second disk optional //
+   #disk { 
+   # label = "disk1"
+   # unit_number = 1
+   # size = var.second_disk_size[terraform.workspace]
+   #}
 
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
@@ -84,6 +90,12 @@ resource "vsphere_virtual_machine" "vm" {
         "sudo chmod 700 /home/NEW_USER/.ssh",
         "sudo chmod 600 /home/NEW_USER/.ssh/authorized_keys",
         "echo 'ersinkaya ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers"
+       # //second_disk opsiyonel// 
+       # "echo -e 'o\nn\np\n1\n\n\nt\n8e\np\nw' | sudo fdisk /dev/sdb",
+       # "sudo pvcreate /dev/sdb1",
+       # "sudo vgextend ubuntu-vg /dev/sdb1",
+       # "sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv",
+       # "sudo resize2fs /dev/ubuntu-vg/ubuntu-lv"
    ]
 
      connection{
